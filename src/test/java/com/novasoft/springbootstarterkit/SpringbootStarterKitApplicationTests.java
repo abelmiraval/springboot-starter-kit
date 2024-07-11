e7f0db3438c3ce422d6ff19d9b59a8e8508f957c
abelmiraval <abel.miraval.rp@viaexperis2.pe>
2024-07-11 15:52:24 -0500

    Init project

---
+++ src/test/java/com/novasoft/springbootstarterkit/SpringbootStarterKitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.novasoft.springbootstarterkit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootStarterKitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
